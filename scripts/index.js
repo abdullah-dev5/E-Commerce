d32bb1c4bbe41cf63bc55e8435984ce83fd7a5f8
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-07-29 10:10:03 +0500

    [+] added cart counter

--- scripts/index.js
+++ scripts/index.js
@@ -162,9 +162,9 @@ function addCart() {
       quantity: 1,
     };
 
-    selectedItem['title'] = txtContainer.find(".heading").text();
+    selectedItem['title'] = txtContainer.find(".heading").text(); 
     selectedItem['brand'] = txtContainer.find(".brand").text();
-    selectedItem['price'] = parseFloat(txtContainer.find(".price").text().substring(1));
+    selectedItem['price'] = parseFloat(txtContainer.find(".price").text().substring(1)); 
     selectedItem['imgUrl'] = card.find(".img-container img").attr("src");
     chosenProducts.push(selectedItem);
     showCartProducts();
@@ -175,8 +175,14 @@ function addCart() {
 
 //? this functions loads all the user selected products into the cart. 
 function showCartProducts() {
-  let totalItemCount = chosenProducts.length;
+  // Set the counter to 0
+  let totalItemCount = 0;
   let totalPrice = 0;
+
+  // count the quantity of all the products available in the cart.
+  chosenProducts.forEach((item)=>{
+    totalItemCount += parseInt(item.quantity);
+  });
 
   // clear the previous result in cart container
   $("#cart-products-container").empty();
@@ -188,7 +194,9 @@ function showCartProducts() {
   });
 
   $("#total-products").text(totalItemCount); // display total products in the cart
+  $("#cart-counter").text(totalItemCount);
   $("#total-price").text(`$${totalPrice.toFixed(2)}`); // display sum of total amount of user selected products
+
 }
 
 //? Builds cards for the Cart Section. 
